eea26ba7d7e014b731339749e1b9445ef88096bc
gml9812 <hee.jun.yun@gmail.com>
2022-03-07 13:48:03 +0900

    feat: core component 및 예제 추가

---
+++ src/js/core/Component.js
@@ -0,0 +1,27 @@
+export default function Component($target, $props) {
+  this.setup = () => {};
+
+  this.setEvent = () => {};
+
+  this.template = () => {
+    return '';
+  };
+
+  this.mounted = () => {};
+
+  this.render = () => {
+    this.$target.innerHTML = this.template();
+    this.mounted();
+  };
+
+  this.setState = (newState) => {
+    this.$state = { ...this.$state, ...newState };
+    this.render();
+  };
+
+  this.$target = $target;
+  this.$props = $props;
+  this.setup();
+  this.setEvent();
+  this.render();
+}
